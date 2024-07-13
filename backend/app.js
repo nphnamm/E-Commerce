b71d69171f503e39c39d2bf1aabf988d08a40c7e
nphnamm <80657434+nphnamm@users.noreply.github.com>
2024-07-13 23:05:30 +0700

    backend

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.use(express.json());
+// CONFIG
+if(process.env.NODE_ENV !== "PRODUCTION"){
+    require("dotenv").config({path: "backend/config/.env"});
+}
+// app.use(express.json());
 
-// Route Imports
-const product = require("./routes/productRoute");
-app.use("api/v1", product);
+// // Route Imports
+// const product = require("./routes/productRoute");
+// app.use("api/v1", product);
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,34 @@
 const app = require("./app");
 
-const dotenv = require("dotenv");
 
-//config
+// handling uncaught exception 
+process.on("uncaughtException", (err)=>{
+    console.log(`Error: ${err.message}`);
+    console.log(`Shutting down the server for handling uncaught exception`);
 
-dotenv.config({path:"backend/config/config.env"});
-
-
-
-app.listen(process.env.PORT,()=>{
-    console.log("Server is running on port "+process.env.PORT);
+    
 })
+
+if(process.env.NODE_ENV !== "PRODUCTION"){
+    require("dotenv").config({path: "backend/config/.env"});
+}
+
+// const dotenv = require("dotenv");
+
+// //config
+
+// dotenv.config({path:"backend/config/config.env"});
+
+
+
+// app.listen(process.env.PORT,()=>{
+//     console.log("Server is running on port "+process.env.PORT);
+// })
+
+const server = app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on port ${process.eventNames.PORT}`)
+})
+process.on('unhabledRejection', (err)=>{{
+    console.log(`Shutting down the server for ${error.message}`);
+    console.log(`Shutting down the server for unhandle Promise rejection`);
+}})
