da7a35966fc5c693c8c60412343d5128b26f7b16
nphnamm <80657434+nphnamm@users.noreply.github.com>
2024-07-15 21:22:20 +0700

    add schema user

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const app = express();
+const ErrorHandler = require("./utils/ErrorHandler");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(fileUpload({useTempFiles:true}));
 
 
 
@@ -14,5 +22,5 @@ if(process.env.NODE_ENV !== "PRODUCTION"){
 // // Route Imports
 // const product = require("./routes/productRoute");
 // app.use("api/v1", product);
-
+app.use(ErrorHandler);
 module.exports = app;
