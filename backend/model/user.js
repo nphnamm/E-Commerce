498fbca80ca1eb0289518e6f028b387aa7dae0f3
nphnamm <80657434+nphnamm@users.noreply.github.com>
2024-07-15 21:30:26 +0700

    config multer

---
+++ backend/model/user.js
@@ -0,0 +1,83 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String, 
+        required: [true, "Please enter your name"],
+
+    },
+    email: {
+        type: String, 
+        required: [true,"Please enter your email!"],
+    }, 
+    password:{
+        type:String, 
+        required: [true,"Please enter your password"],
+        minLength: [4, "Password should be greater than 4 characters"],
+        select: false,
+    }, 
+    phoneNumber: {
+        type: Number
+    },
+    addresses:[
+        {
+            country:{
+                type:String,
+            },
+            city:{
+                type:String,
+            },
+            address1:{
+                type:String,
+            },
+            address2:{
+                type:String,
+            },
+            zipCode:{
+                type:Number,
+            },
+            addressType:{
+                type:String,
+            }
+
+        }
+    ],
+    role:{
+        type:String,
+        default:"user"
+    },
+    avatar:{
+        public_id:{
+            type:String,
+            required: true,
+        },
+        url:{
+            type: String, 
+            required: true
+        }
+    }, 
+    createAt:{
+        type: Date, 
+        default: Date.now(),
+    }, 
+    resetPasswordToken:String,
+    resetPasswordTime: Date
+});
+
+//TODO HASH PASSWORD
+
+userSchema.methods.getJwtToken = function () {
+    return jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE,
+        });
+
+};
+
+//TODO COMPARE PASSWORD
+userSchema.methods.comparaPassword = async function (enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password);
+
+}
+module.exports = mongoose.model("User", userSchema);
