5fea8f232e059c6aa5a5993d5c780a2b86f679ad
nphnamm <80657434+nphnamm@users.noreply.github.com>
2024-07-19 23:00:10 +0700

    send email

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -5,17 +5,23 @@ const User = require("../model/user");
 const {upload} = require('../multer');
 const ErrorHandler = require("../utils/ErrorHandler");
 const fs = require("fs");
-
+const sendMail = require("../utils/sendMail");
+const jwt = require("jsonwebtoken");
 
 router.post("/create-user", upload.single("file"), async(req,res,next)=>{
+    
+    try {
     const {name, email,password} = req.body;
-
 
     const userEmail = await User.findOne({email});
 
     if(userEmail){
         const filename = req.file.filename;
         const filePath = `uploads/${filename}`;
+
+        // fs.unlink is a Node. js function used to delete a file from the filesystem. 
+        // It's part of the fsmodule, which provides an interface for 
+        //interacting with the file system.
         fs.unlink(filePath, (err)=>{
             if(err){
                 console.log(err);
@@ -39,6 +45,29 @@ router.post("/create-user", upload.single("file"), async(req,res,next)=>{
         avatar: fileUrl
 
     };
+    const activationToken = createActivationToken(user);
+    const activationUrl = `http://localhost:3000/activation/${activationToken}`;
+
+    console.log('check', activationUrl);
+    try {
+        await sendMail({
+            email: user.email,
+            subject: "Activate your account",
+            message: `Hello ${user.name}, please click on the link to active your account: ${activationUrl}`,
+
+        })
+        res.status(201).json({
+            success:true,
+            message: `please check your email: -${user.email} to activate your account`
+        })
+
+    }catch(error){
+        console.log("Error", error);
+        return next(new ErrorHandler(error.message,500));
+
+    }
+
+
     console.log(user);
     const newUser = await User.create(user);
     res.status(201).json({
@@ -46,8 +75,16 @@ router.post("/create-user", upload.single("file"), async(req,res,next)=>{
         newUser,
 
     })
+    } catch (error) {
+    return next(new ErrorHandler(error.message, 500));
+    }
 
+});
 
-})
+// Crete Activation Token
+const createActivationToken = (user) =>{
+    return jwt.sign(user,process.env.ACTIVATION_SECRET,{expiresIn:"5m"});
+}
+
 
 module.exports = router;
