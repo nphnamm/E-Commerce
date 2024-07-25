96f7c0fb5b243f47db9a88884ec37bf0118b9601
nphnamm <80657434+nphnamm@users.noreply.github.com>
2024-07-25 22:25:46 +0700

    add product

--- frontend/src/components/Route/BestDeals/BestDeals.jsx
+++ frontend/src/components/Route/BestDeals/BestDeals.jsx
@@ -1,41 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import styles from '../../../styles/styles'
-import { productData } from '../../../static/data';
-import {useSelector} from "react-redux";
+import React, { useEffect, useState } from "react";
+import styles from "../../../styles/styles";
+import { productData } from "../../../static/data";
+import { useSelector } from "react-redux";
+import ProductCard from "../ProductCard/ProductCard";
 const BestDeals = () => {
-  const [data,setData] = useState([]);
-  const {allProducts} = useSelector((state) =>state.products);
-  useEffect(()=>{
-    const allProductsData = allProducts ? [...allProducts] : [];
-    const sortedData = allProductsData?.sort((a,b)=>b.sold_out- a.sold_out);
-    const firstFive = sortedData && productData.slice(0,5);
-    setData(firstFive)
-
-})
-    return (
-    <div> 
-      Best Deal
-        <div className={`${styles.section}`}>
-            <div className={`${styles.heading}`}>
-                <h1>Best Deals</h1>
-            </div>
-            <div className='grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-2 border-0'>
-                {
-                    data && data.length !== 0 && (
-                        
-                        <>
-
-                            {data && data.map((i,index)=> <ProductCard data={i} key={index}/>)}
-                        
-                        </> 
-                    )
-                }
-
-            </div>
-
+  const [data, setData] = useState([]);
+//   const { allProducts } = useSelector((state) => state.products);
+  useEffect(() => {
+    // const allProductsData = allProducts ? [...allProducts] : [];
+    // const sortedData = allProductsData?.sort((a, b) => b.sold_out - a.sold_out);
+    // const firstFive = sortedData && productData.slice(0, 5);
+    const d = productData && productData.sort((a,b)=> b.total_sell - a.total_sell);
+    const firstFive = d.slice(0,5);
+    setData(firstFive);
+  }, []);
+  console.log('product card', productData);
+  return (
+    <div>
+   
+      <div className={`${styles.section}`}>
+        <div className={`${styles.heading}`}>
+          <h1>Best Deals</h1>
         </div>
+        <div className="grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-4 lg:gap-[25px] xl:grid-cols-5 xl:gap-[30px] mb-2 border-0">
+          {data && data.length !== 0 && (
+            <>
+              {data &&
+                data.map((i, index) => <ProductCard data={i} key={index} />)}
+            </>
+          )}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default BestDeals
+export default BestDeals;
