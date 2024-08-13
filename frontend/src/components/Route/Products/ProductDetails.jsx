83c89a3d3219efafdfd725882724cbac12967f82
nphnamm <80657434+nphnamm@users.noreply.github.com>
2024-08-13 15:30:01 +0700

    productDetail, Product Reviews, Seller Information Frontend Part

--- frontend/src/components/Route/Products/ProductDetails.jsx
+++ frontend/src/components/Route/Products/ProductDetails.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styles from "../../../styles/styles";
-import { AiFillHeart, AiOutlineHeart, AiOutlineMessage, AiOutlineShoppingCart } from "react-icons/ai";
+import {
+  AiFillHeart,
+  AiOutlineHeart,
+  AiOutlineMessage,
+  AiOutlineShoppingCart,
+} from "react-icons/ai";
 
 const ProductDetails = ({ data }) => {
   const [count, setCount] = useState(1);
@@ -73,8 +78,7 @@ const ProductDetails = ({ data }) => {
               <div className="w-full 800px:w-[50%]">
                 {/* //TODO: PRODUCT TITLE */}
                 <h1 className={`${styles.productTitle}`}>{data.name}</h1>
-                
-                
+
                 {/* //TODO: PRODUCT DESCRIPTION */}
 
                 <p>{data.description}</p>
@@ -132,50 +136,194 @@ const ProductDetails = ({ data }) => {
 
                 {/* //TODO: PRODUCT ADD BUTTON*/}
 
-
-                <div className={`${styles.button} mt-6 !rounded !h-11 flex items-center`}>
-                    <span className="text-white flex items-center">
-                      Add to cart <AiOutlineShoppingCart className="ml-1"/>
-
-                    </span>
-
+                <div
+                  className={`${styles.button} mt-6 !rounded !h-11 flex items-center`}
+                >
+                  <span className="text-white flex items-center">
+                    Add to cart <AiOutlineShoppingCart className="ml-1" />
+                  </span>
                 </div>
 
                 {/* //TODO: PRODUCT PRODUCER */}
                 <div className="flex items-center pt-8">
-                    <img
-                      src={data.shop.shop_avatar.url}
-                      alt=""
-                      className="w-[50px] h-[50px] rounded-full mr-2"
-                    />
-                    <div className="pr-8">
-                      <h3 className={`${styles.shop_name} pb-1 pt-1`}>
-                        {data.shop.name}
-
-
-                      </h3>
-                      <h5 className="pb-3 text-[15px]">
-                        ({data.shop.ratings}) Ratings
-
-                      </h5>
-                    </div>
-                    <div 
+                  <img
+                    src={data.shop.shop_avatar.url}
+                    alt=""
+                    className="w-[50px] h-[50px] rounded-full mr-2"
+                  />
+                  <div className="pr-8">
+                    <h3 className={`${styles.shop_name} pb-1 pt-1`}>
+                      {data.shop.name}
+                    </h3>
+                    <h5 className="pb-3 text-[15px]">
+                      ({data.shop.ratings}) Ratings
+                    </h5>
+                  </div>
+                  <div
                     className={`${styles.button} bg-[#6443d1] mt-1 !rounded !h-11`}
                     onClick={handleMessageSubmit}
-                    >
-                          <span className="text-white flex items-center">
-                            Send Message <AiOutlineMessage className="ml-1"/>
-
-                          </span>
-                    </div>
-
+                  >
+                    <span className="text-white flex items-center">
+                      Send Message <AiOutlineMessage className="ml-1" />
+                    </span>
+                  </div>
                 </div>
-
               </div>
             </div>
           </div>
+          <ProductDetailsInfo data={data} />
+          <br />
+          <br />
         </div>
       ) : null}
+    </div>
+  );
+};
+const ProductDetailsInfo = ({ data }) => {
+  const [active, setActive] = useState(1);
+  return (
+    <div className="bg-[#f5f6fb] px-3 800px:px-10 py-2 rounded h-[40vh]">
+      <div className="w-full flex justify-between border-b pt-10 pb-2">
+        <div className="relative">
+          <h5
+            className={
+              "text-[#000] text-[18px] px-1 leading-5 font-[600] cursor-pointer 800px:text-[20px]"
+            }
+            onClick={() => setActive(1)}
+          >
+            Product Details
+          </h5>
+          {active === 1 ? (
+            <div className={`${styles.active_indicator}`} />
+          ) : null}
+        </div>
+
+        <div className="relative">
+          <h5
+            className={
+              "text-[#000] text-[18px] px-1 leading-5 font-[600] cursor-pointer 800px:text-[20px]"
+            }
+            onClick={() => setActive(2)}
+          >
+            Product Reviews
+          </h5>
+          {active === 2 ? (
+            <div className={`${styles.active_indicator}`} />
+          ) : null}
+        </div>
+
+        <div className="relative">
+          <h5
+            className={
+              "text-[#000] text-[18px] px-1 leading-5 font-[600] cursor-pointer 800px:text-[20px]"
+            }
+            onClick={() => setActive(3)}
+          >
+            Seller Information
+          </h5>
+          {active === 3 ? (
+            <div className={`${styles.active_indicator}`} />
+          ) : null}
+        </div>
+      </div>
+
+      {active === 1 ? (
+        <>
+          <p className="py-2 text-[18px] leading-8 pb-10 whitespace-pre-line">
+            Product details are a crucial part of any eCommerce website or
+            online marketplace. These details help the potential customers to
+            make an informed decision about the product they are interested in
+            buying. A well-written product description can also be a powerful
+            marketing tool that can help to increase sales.Product details
+            typically include information about the product's features,
+            specifications, dimensions, weight, materials, and other relevant
+            information that can help customers to understand the product
+            better. The product details section should also include high-quality
+            images and videos of the product, as well as customer reviews and
+            ratings
+          </p>
+          <p className="py-2 text-[18px] leading-9 pb-10 whitespace-pre-line">
+            Product details are a crucial part of any eCommerce website or
+            online marketplace. These details help the potential customers to
+            make an informed decision about the product they are interested in
+            buying. A well-written product description can also be a powerful
+            marketing tool that can help to increase sales
+          </p>
+
+          <p className="py-2 text-[18px] leading-9 pb-10 whitespace-pre-line">
+            Product details are a crucial part of any eCommerce website or
+            online marketplace. These details help the potential customers to
+            make an informed decision about the product they are interested in
+            buying. A well-written product description can also be a powerful
+            marketing tool that can help to increase sales
+          </p>
+        </>
+      ) : null}
+
+      {active === 2 ? (
+       
+          <div className="w-full justify-center min-h-[40vh] flex items-center">
+            <p>No Reviews Yet!</p>
+          </div>
+
+      ) : null}
+       {active === 3 ? (
+       
+       <div className="w-full block 800px:flex p-5">
+         <div className="w-full 800px:w-[50%]">
+            <div className="flex items-center">
+              <img
+              src={data.shop.shop_avatar.url}
+              className="w-[50px] h-[50px] rounded-full"
+              alt=""
+              />
+              <div className="pl-3">
+                <h3 className={`${styles.shop_name}`}>
+                  {data.shop.name}
+
+                </h3>
+                <h5 className="pb-3 text-[15px]">
+                    {data.shop.ratings} Ratings
+                </h5>
+              </div>
+            </div>
+            <p className="pt-2">
+              Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+              Lorem Ipsum has been the industry's standard dummy text ever since the 
+              1500s,
+          </p>
+
+         </div>
+        <div className="w-full 800px:w-[50%] mt-5 800px:mt-0 800px:flex flex-col items-end">
+            <div className="text-left">
+              <h5 className="font-[600]">
+                Joined on <span className="font-[500]"> 14 March, 2023</span>
+
+              </h5>
+              <h5 className="font-[600] pt-3">
+                Total Products <span className="font-[500]">1,223</span>
+
+              </h5>
+
+              <h5 className="font-[600] pt-3">
+                Total Reviews <span className="font-[500]">324</span>
+
+              </h5>
+              <Link to="/">
+                <div className={`${styles.button} !rounded-[4px] !h-[39.5px] mt-3`}>
+                  <h4 className="text-white">
+                    Visit Shop
+                  </h4>
+
+                </div>
+              </Link>
+
+            </div>
+        </div>
+
+       </div>
+
+   ) : null}
     </div>
   );
 };
