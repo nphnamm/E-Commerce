bb00b547f247387b09909dcde02d21590f33326c
nphnamm <80657434+nphnamm@users.noreply.github.com>
2024-08-19 16:15:00 +0700

    shop create

--- backend/app.js
+++ backend/app.js
@@ -45,7 +45,11 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 
 //import routes
 const user= require("./controller/user");
+const shop= require("./controller/shop");
+
 app.use("/api/v2/user",user);
+app.use("/api/v2/shop",shop);
+
 
 app.use(ErrorHandler);
 
--- backend/controller/shop.js
+++ backend/controller/shop.js
@@ -27,6 +27,31 @@ router.post('/create-shop',upload.single("file"),async(req,res,next)=>{
             return next(new ErrorHandler("Shop already exists",400));
 
         }
+        const filename = req.file.filename;
+        const fileUrl = path.join(filename);
+        const seller = {
+          name: name,
+          email: email,
+          password: password,
+          avatar: fileUrl,
+        };
+        const activationToken = createActivationToken(seller);
+        const activationUrl = `http://localhost:3000/seller/activation/${activationToken}`;
+        console.log("check", activationUrl);
+        try {
+            await sendMail({
+              email: seller.email,
+              subject: "Activate your shop",
+              message: `Hello ${seller.name}, please click on the link to active your account: ${activationUrl}`,
+            });
+            res.status(201).json({
+              success: true,
+              message: `please check your email: ${seller.email} to activate your shop`,
+            });
+          } catch (error) {
+            console.log("Error", error);
+            return next(new ErrorHandler(error.message, 500));
+          }
 
 
     }catch(eror){
@@ -34,4 +59,52 @@ router.post('/create-shop',upload.single("file"),async(req,res,next)=>{
 
     }
 })
+
+// create activation token
+const createActivationToken = (seller) => {
+    return jwt.sign(seller, process.env.ACTIVATION_SECRET, {
+      expiresIn: "5m",
+    });
+};
+
+// activate user
+router.post(
+    "/activation",
+    catchAsyncErrors(async (req, res, next) => {
+      try {
+        const { activation_token } = req.body;
+  
+        const newSeller = jwt.verify(
+          activation_token,
+          process.env.ACTIVATION_SECRET
+        );
+  
+        if (!newSeller) {
+          return next(new ErrorHandler("Invalid token", 400));
+        }
+        const { name, email, password, avatar, zipCode, address, phoneNumber } =
+          newSeller;
+  
+        let seller = await Shop.findOne({ email });
+  
+        if (seller) {
+          return next(new ErrorHandler("User already exists", 400));
+        }
+  
+        seller = await Shop.create({
+          name,
+          email,
+          avatar,
+          password,
+          zipCode,
+          address,
+          phoneNumber,
+        });
+  
+        sendShopToken(seller, 201, res);
+      } catch (error) {
+        return next(new ErrorHandler(error.message, 500));
+      }
+    })
+  );
 module.exports = router;
