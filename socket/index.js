ac6fd0809559e6446df54851c2fd43f52a9d0f7d
nphnamm <80657434+nphnamm@users.noreply.github.com>
2024-09-11 00:20:09 +0700

    socket

--- socket/index.js
+++ socket/index.js
@@ -10,8 +10,41 @@ require("dotenv").config({
   });
 app.use(cors());
 app.get("/",(req,res)=>{
-    res.send("Hello world!");
+    res.send("Hello world from socket server!");
+});
+
+let users =[];
+
+const addUser = (userId, socketId) =>{
+    !users.some((user) => user.userId === userId) && users.push({ userId, socketId});
+
+}
+
+// Define a message object with a seen property 
+
+const createMessage = ({senderId, receivedId,text, images})=>({
+
+    senderId,
+    receivedId,
+    text,
+    images,
+    seen:false
 })
+
+io.on("connection", (socket)=>{
+    // when connect
+    console.log(`a user is connected`);
+
+    // take userId and socketId from user
+    socket.on("addUser", (userId) => {
+        addUser(userId, socket.id);
+        io.emit("getUsers", users);
+        
+    })
+})
+
+
+
 server.listen(process.env.PORT,()=>{
     console.log(`server is running on port ${process.env.PORT}`)
 })
