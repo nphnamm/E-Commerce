0af25cf438bc2068b06f1158e511496ab934e1b1
NamNPH <80657434+nphnamm@users.noreply.github.com>
2024-11-12 12:14:03 +0700

    modify project

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,8 @@ process.on("uncaughtException", (err) => {
   console.log(`Shutting down the server for handling uncaught exception`);
 });
 
-if (process.env.NODE_ENV !== "PRODUCTION") {
-  require("dotenv").config({ path: "config/.env" });
+if (process.env.NODE_ENV === "PRODUCTION") {
+  require("dotenv").config({ path: ".env" });
 }
 
 // const dotenv = require("dotenv");
